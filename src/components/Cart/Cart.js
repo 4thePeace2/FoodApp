25f863c97dfc677b620000b3ff62a4fba02b64b5
4thePeace2 <milosmaljen@gmail.com>
2021-04-21 22:57:35 +0200

    added some useEffect

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -11,9 +11,13 @@ const Cart = (props) => {
   const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
   const hasItems = cartCtx.items.length > 0;
 
-  const cartItemsRemoveHandler = (id) => {};
+  const cartItemsRemoveHandler = (id) => {
+    cartCtx.removeItem(id);
+  };
 
-  const cartItemsAddHandler = (item) => {};
+  const cartItemsAddHandler = (item) => {
+    cartCtx.addItem({...item, amount: 1});
+  };
 
   const cartItems = (
     <ul className={styles["cart-items"]}>
